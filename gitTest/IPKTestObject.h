e3730912c98ca19c8460f342b1d7ed228ed5a080
ilbumy <ilbumy@hotmail.com>
2013-10-17 21:58:48 -0400

    added a test class

---
+++ gitTest/IPKTestObject.h
@@ -0,0 +1,13 @@
+//
+//  IPKTestObject.h
+//  gitTest
+//
+//  Created by InTouch Administrator on 10/17/13.
+//  Copyright (c) 2013 Ilbum Kwak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IPKTestObject : NSObject
+
+@end
---
+++ gitTest/IPKTestObject.m
@@ -0,0 +1,13 @@
+//
+//  IPKTestObject.m
+//  gitTest
+//
+//  Created by InTouch Administrator on 10/17/13.
+//  Copyright (c) 2013 Ilbum Kwak. All rights reserved.
+//
+
+#import "IPKTestObject.h"
+
+@implementation IPKTestObject
+
+@end
